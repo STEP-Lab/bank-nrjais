37599d92f6a507df6c046b546d6c4f9f937cb6ee
Neeraj Jaiswal <neerajj85@gmail.com>
2018-03-19 17:39:31 +0530

    Added functionality to check balance

---
+++ library/src/main/java/com/step/bank/Account.java
@@ -0,0 +1,15 @@
+package com.step.bank;
+
+public class Account {
+  private final String accNumber;
+  private int balance;
+
+  public Account(String accNumber, int balance) {
+    this.accNumber = accNumber;
+    this.balance = balance;
+  }
+
+  public int getBalance() {
+    return balance;
+  }
+}
